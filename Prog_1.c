3080accbe4607f00da2fca3a5ab2b4b7a397c515
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2018-04-30 16:11:02 +1000

    Added semaphores to get the threads executing in the correct order.

--- Prog_1.c
+++ Prog_1.c
@@ -5,58 +5,81 @@
 #include <string.h>
 #include <sys/types.h>
 
-/* Mutual exclusion object */
-pthread_mutex_t mutex;
+typedef struct {
+    int *fd_write;
+    char const * const file;
+} reading_arguments_t;
 
-/* Semaphores */
-sem_t one, two, master;
-pthread_t tidA,tidB,tidC; //Thread ID
-pthread_attr_t attr; //Set of thread attributes
+sem_t *readSem, *passSem, *writeSem; // Semaphores
+pthread_t tidA, tidB, tidC; // Thread IDs
+pthread_attr_t attr; // Set of thread attributes
 
 void *readData(); // Reads from data.txt
 void *passData(); // Passes to ThreadC
 void *writeData(); // Writes content to src.text
-void initializeData();
+void initializeData(); // Initializes Data
 
 int main(int argc, char*argv[])
 {
-    printf("*****START OF PROG_1*****\n");
+    printf("***** START OF PROG_1 *****\n");
     
     initializeData();
     
     pthread_attr_init(&attr); // Get the default attributes
     
     pthread_create(&tidA, &attr, readData, NULL); // Creates thread A
-    
     pthread_create(&tidB, &attr, passData, NULL); // Creates thread B
-    
     pthread_create(&tidC, &attr, writeData, NULL); // Creates thread C
     
-    // send semaphore to thread A to begin the running of threads
+    sem_post(readSem); // Begin reading thread and the execution of the program
     
-    // wait for threads to exit
+    if(pthread_join(tidA, NULL) != 0)
+        printf("Issue with Thread A\n");
+    if(pthread_join(tidB, NULL) != 0)
+        printf("Issue with Thread B\n");
+    if(pthread_join(tidC, NULL) != 0)
+        printf("Issue with Thread C\n");
     
-    printf("*****END OF PROG_1****\n");
+    printf("****** END OF PROG_1 *****\n");
 }
 
 /* Thread A will begin control in this function*/
 void *readData()
 {
-    
+    printf("%i", sem_wait(readSem)); // sem_wait for read semaphore
+    printf("Reading Data \n");
+    // read one line from data.txt
+    // write into the pipe
+    sem_post(passSem); // sem_post for pass semaphore
 }
 
 /* Thread B will begin control in this function*/
 void *passData()
 {
-
+    printf("%i", sem_wait(passSem)); // sem_wait for pass semaphore
+    printf("Passing Data \n");
+    // read from the pipe
+    // pass to thread C, the method of passing the data is unspecified
+    sem_post(writeSem); // sem_post for write semaphore
 }
 
 /* Thread C will begin control in this function*/
 void *writeData()
 {
     
+    printf("%i", sem_wait(writeSem)); // sem_wait for write semaphore
+    printf("Writing Data \n");
+    // read from wherever...
+    // Determine whether it is from content or header
+    // write content to src.txt
+    // if last eof then exit. otherwise sem_post(readSem)
+    
 }
 
 void initializeData() {
-
+    readSem = sem_open("/readSem", O_CREAT, 0644, 1);
+    passSem = sem_open("/passSem", O_CREAT, 0644, 1);
+    writeSem = sem_open("/writeSem", O_CREAT, 0644, 1);
+    
+    pthread_attr_init(&attr);
 }
