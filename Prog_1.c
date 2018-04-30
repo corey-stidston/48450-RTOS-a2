81cf4de8faac88091d89b798a2a2150f64052a8c
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2018-04-30 22:28:32 +1000

    Added structures to hold the data that each thread should use, thread A can read a line from data.txt and write to the pipe, thread b can read from the pipe and write to a shared buffer, thread c can read from the shared buffer, determine where the content region starts and write to src.txt. On completion the program saves its runtime information to shared memory under the name 'shared'.

--- Prog_1.c
+++ Prog_1.c
@@ -4,82 +4,193 @@
 #include <semaphore.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+// Note: use -pthread and -lrt to compile
+
+#define BUFFER_SIZE 1024
+#define DATA_FILENAME "data.txt"
+#define SRC_FILENAME "src.txt"
+#define END_OF_HEADER "end_header"
+#define SHARED_MEMORY_NAME "shared"
 
 typedef struct {
-    int *fd_write;
-    char const * const file;
-} reading_arguments_t;
+    char buffer[BUFFER_SIZE];
+} buffer_t;
 
-sem_t *readSem, *passSem, *writeSem; // Semaphores
+typedef struct {
+    int *fd;
+    char const * const filename;
+} reading_args_t;
+
+typedef struct {
+    int *fd;
+    buffer_t *shared_buffer;
+} passing_args_t;
+
+typedef struct {
+    buffer_t *shared_buffer;
+    char const * const filename;
+} writing_args_t;
+
+sem_t readSem, passSem, writeSem; // Semaphores
 pthread_t tidA, tidB, tidC; // Thread IDs
-pthread_attr_t attr; // Set of thread attributes
+pthread_attr_t attr; // Thread attributes
 
 void *readData(); // Reads from data.txt
 void *passData(); // Passes to ThreadC
 void *writeData(); // Writes content to src.text
 void initializeData(); // Initializes Data
 
+void writeRunningTimetoSharedMemory(int runningTimeInMilliseconds)
+{
+    int shm_fd = shm_open(SHARED_MEMORY_NAME, O_CREAT | O_RDWR, 0666);
+    
+    ftruncate(shm_fd, sizeof(int));
+    void *ptr = mmap(0, sizeof(int), PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    
+    sprintf(ptr, "%i", runningTimeInMilliseconds);
+}
+
 int main(int argc, char*argv[])
 {
-    printf("***** START OF PROG_1 *****\n");
+    struct timeval start, end;
+    
+    gettimeofday(&start, NULL);
+    
+    int fd[2];
+    
+    if(pipe(fd) < 0)
+    {
+        perror("Pipe error");
+        exit(0);
+    }
     
     initializeData();
     
+    // Initialize data structures for each thread
+    buffer_t sharedBuffer = {0};
+    reading_args_t reading_args = {fd, DATA_FILENAME};
+    passing_args_t passing_args = {fd, &sharedBuffer};
+    writing_args_t writing_args = {&sharedBuffer, SRC_FILENAME};
+    
     pthread_attr_init(&attr); // Get the default attributes
     
-    pthread_create(&tidA, &attr, readData, NULL); // Creates thread A
-    pthread_create(&tidB, &attr, passData, NULL); // Creates thread B
-    pthread_create(&tidC, &attr, writeData, NULL); // Creates thread C
-    
-    sem_post(readSem); // Begin reading thread and the execution of the program
+    pthread_create(&tidA, &attr, readData, &reading_args); // Creates thread A
+    pthread_create(&tidB, &attr, passData, &passing_args); // Creates thread B
+    pthread_create(&tidC, &attr, writeData, &writing_args); // Creates thread C
     
     if(pthread_join(tidA, NULL) != 0)
-        printf("Issue with Thread A\n");
+        printf("Issue joining Thread A\n");
     if(pthread_join(tidB, NULL) != 0)
-        printf("Issue with Thread B\n");
+        printf("Issue joining Thread B\n");
     if(pthread_join(tidC, NULL) != 0)
-        printf("Issue with Thread C\n");
+        printf("Issue joining Thread C\n");
+        
+    gettimeofday(&end, NULL);
+    int mtime = ((end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_usec - start.tv_usec)/1000.0);
+    printf("Elapsed Time: %i milliseconds\n", mtime);
     
-    printf("****** END OF PROG_1 *****\n");
+    writeRunningTimetoSharedMemory(mtime);
+    
+    return(0);
 }
 
-/* Thread A will begin control in this function*/
-void *readData()
+/* Thread A will begin control in this function */
+void *readData(void *param)
 {
-    printf("%i", sem_wait(readSem)); // sem_wait for read semaphore
-    printf("Reading Data \n");
-    // read one line from data.txt
-    // write into the pipe
-    sem_post(passSem); // sem_post for pass semaphore
+    reading_args_t *parameters = param;
+    char line[BUFFER_SIZE];
+    
+    FILE* readFile = fopen(parameters->filename, "r");
+    if(!readFile)
+    {
+        perror("Invalid File\n");
+        exit(0);
+    }
+    
+    while(!sem_wait(&readSem) && fgets(line, BUFFER_SIZE, readFile) != NULL)
+    {
+        write(parameters->fd[1], line, strlen(line)+1); // write into the pipe
+        sem_post(&passSem);
+    }
+
+    /* Close pipe and FILE* */
+    close(parameters->fd[1]);
+    fclose(readFile);
+    
+    /* Cancel the execution of threads */
+    pthread_cancel(tidA);
+    pthread_cancel(tidB);
+    pthread_cancel(tidC);
 }
 
-/* Thread B will begin control in this function*/
-void *passData()
+/* Thread B will begin control in this function */
+void *passData(void *param)
 {
-    printf("%i", sem_wait(passSem)); // sem_wait for pass semaphore
-    printf("Passing Data \n");
-    // read from the pipe
-    // pass to thread C, the method of passing the data is unspecified
-    sem_post(writeSem); // sem_post for write semaphore
+    passing_args_t *parameters = param;
+    
+    while(!sem_wait(&passSem))
+    {
+        read(parameters->fd[0], parameters->shared_buffer, BUFFER_SIZE); // read from the pipe
+        sem_post(&writeSem);
+    }
+    
+    close(parameters->fd[0]); // Close pipe
 }
 
-/* Thread C will begin control in this function*/
-void *writeData()
+/* Thread C will begin control in this function */
+void *writeData(void *param)
 {
+    writing_args_t *parameters = param;
+    FILE* writeFile = fopen(parameters->filename, "w");
+    if(!writeFile)
+    {
+        perror("Invalid File\n");
+        exit(0);
+    }
     
-    printf("%i", sem_wait(writeSem)); // sem_wait for write semaphore
-    printf("Writing Data \n");
-    // read from wherever...
-    // Determine whether it is from content or header
-    // write content to src.txt
-    // if last eof then exit. otherwise sem_post(readSem)
+    char line[BUFFER_SIZE];
+    int eoh_flag = 0;
     
+    while(!sem_wait(&writeSem))
+    {
+        if(eoh_flag)
+        {
+            fputs(parameters->shared_buffer->buffer, writeFile);
+        }
+        else if(strstr(parameters->shared_buffer->buffer, END_OF_HEADER))
+        {
+            eoh_flag = 1;
+        }
+
+        sem_post(&readSem);
+    }
+    
+    fclose(writeFile); // Close FILE*
 }
 
 void initializeData() {
-    readSem = sem_open("/readSem", O_CREAT, 0644, 1);
-    passSem = sem_open("/passSem", O_CREAT, 0644, 1);
-    writeSem = sem_open("/writeSem", O_CREAT, 0644, 1);
+    if(sem_init(&readSem, 0, 1))
+    {
+        perror("Error initializing semaphore.");
+        exit(0);
+    }
     
+    if(sem_init(&passSem, 0, 0))
+    {
+        perror("Error initializing semaphore.");
+        exit(0);
+    }
+    
+    if(sem_init(&writeSem, 0, 0))
+    {
+        perror("Error initializing semaphore.");
+        exit(0);
+    }
+
     pthread_attr_init(&attr);
 }
